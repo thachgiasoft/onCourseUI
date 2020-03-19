81b53e3078d9a616bbf1934afa2b0a853c54edc0
Nikhil <menon.nikhil@icloud.com>
2020-03-18 21:48:44 -0500

    Progress on the AddCourse View

--- onCourseUI/AddCourse/AddCourseView.swift
+++ onCourseUI/AddCourse/AddCourseView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct AddCourseView: View {
+    @State private var name: String = ""
+    @State private var code: String = ""
+    @State private var credits: String = ""
+    
+    @State private var section: String = ""
+    @State private var location: String = ""
+    @State private var time: String = ""
+    
+    
     var body: some View {
-        Text("AddCourseView")
+        
+        
+        
+        NavigationView {
+            
+            VStack {
+                PromptRow(title: "Course Name", placeholder: "Enter Course Name", text: $name)
+                PromptRow(title: "Course Code", placeholder: "Enter Course Name", text: $code)
+                PromptRow(title: "Course Credits", placeholder: "Enter Course Name", text: $credits)
+                
+                PromptRow(title: "Course Section", placeholder: "Enter Course Name", text: $section)
+                PromptRow(title: "Course Location", placeholder: "Enter Course Name", text: $location)
+                PromptRow(title: "Course Time", placeholder: "Enter Course Name", text: $time)
+                    .padding(.bottom)
+                
+                Button(action: {
+                    // Do stuff
+                }) {
+                    // Button Layout
+                    Text("Add Course")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .font(.headline)
+                        .cornerRadius(10)
+                }
+                    Spacer()
+                    
+                .navigationBarTitle(Text("Add Course"), displayMode: .automatic)
+            }
+            
+        }
+        
+        
+        
+        
     }
 }
 
---
+++ onCourseUI/AddCourse/PromptRow.swift
@@ -0,0 +1,38 @@
+//
+//  PromptRow.swift
+//  onCourseUI
+//
+//  Created by Nikhil Menon on 3/18/20.
+//  Copyright © 2020 Nikhil Menon. All rights reserved.
+//
+
+import SwiftUI
+
+struct PromptRow: View {
+    var title: String
+    var placeholder: String
+    var text: Binding<String>
+    
+    
+    var body: some View {
+        
+        HStack(alignment: .lastTextBaseline) {
+            Text("\(title): ")
+                .fontWeight(.semibold)
+                .padding(.leading)
+            TextField(placeholder, text: text)
+        }
+        
+        
+    }
+}
+
+struct PromptRow_Previews: PreviewProvider {
+    var text: String = ""
+    
+    static var previews: some View {
+        
+        PromptRow(title: "Course Name", placeholder: "Enter Course Name Here", text: .constant(""))
+            .previewLayout(.sizeThatFits)
+    }
+}
